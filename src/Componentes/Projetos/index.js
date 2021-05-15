7de195c813fc26df41933c620d81b7b00f11fcb1
Fernando Gonçalves Prudente <fernandobug2@gmail.com>
2021-05-15 15:37:06 -0300

    Atualizacao de Projetos

--- src/Componentes/Projetos/index.js
+++ src/Componentes/Projetos/index.js
@@ -6,51 +6,44 @@ function Projetos() {
         <div id='projeto-div'>
 
             <h2>Meus Projetos</h2>
+        
+            <div id='projeto1'>
 
-            <figure id='img-div'>
-                <div>
-                    <h3>Calculadora de IMC</h3>
-                    <p>Eu construir essa calculadora, 
-                        porque eu quis aperfeiçoar  minhas 
-                        habilidade em programação, 
-                        e obter um pouco mais de experiência. 
-                    </p>
-                    <img src='projeto-calculo-imc.png' ></img>
-                    <h3>Tecnologias Usadas</h3>
-                    <p>JavaScript | React.Js | Css</p>
+                <h3>Calculadora IMC</h3>
+                <p>Eu programei essa calculadora IMC, porque eu queria praticar e obter um pouco mais de experiência na programação.
+                </p>
+                <img src='projeto-calculo-imc.png' height='300' width='300'/>
+                <h3>Tecnologias Usadas</h3>
+                <p>| JavaScript | React.js | Css |</p>
+                <a href='https://calculadoraimcfernando.netlify.app' target='_blank' >Link</a>
+            </div>
+                
 
-                    <a href='https://calculadoraimcfernando.netlify.app/' target='_blank'>Link</a>
-                </div>
+            <div id='projeto2'>
+                <h3>Convert Case</h3>
+                <p>Eu programei esse convert case, porque eu queria praticar e obter um pouco mais de experiência na programação.
+                </p>
+                <img src='convert-case-print.png' height='300' width='300'/>
+                <h3>Tecnologias Usadas</h3>
+                <p>| JavaScript | React.js | Css |</p>
+                <a href='https://convertercase.netlify.app'  target='_blank'>Link</a>
+            </div>
 
-                <div id='projeto-meio'>
-                    <h3>Projeto 2</h3>
-                    <p>............................................
-                    ............................................
-                    ............................................
-                    ............................................
-                    </p>
-                    <img src='projeto-cadeado.png' ></img>
-                    <h3>Tecnologias Usadas</h3>
-                    <p>...................</p>
-
-                    <a href=''>Link</a>
-                </div>
-
-                <div>
-                    <h3>Projeto 3</h3>
-                    <p>............................................
-                    ............................................
-                    ............................................
-                    ............................................
-                    </p>
-                    <img src='projeto-cadeado.png' ></img>
-                    <h3>Tecnologias Usadas</h3>
-                    <p>...................</p>
-
-                    <a href=''>Link</a>
-                </div>
-
-            </figure>
+            <div id='projeto3'>
+                <h3>Projeto 3</h3>
+                <p>..................
+                    ..................
+                    ..................
+                </p>
+                <img src='projeto-cadeado.png' height='300' width='300'/>
+                <h3>Tecnologias Usadas</h3>
+                <p>| .................. |
+                    .................. |
+                    .................. |
+                </p>
+                <a href=''  target='_blank'>Link</a>
+            </div>
+            
         </div>
 
     )
